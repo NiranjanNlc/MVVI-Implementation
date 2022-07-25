240dcebbeee3e184d02bdfc7acf20c2d362bcc45
niranjannlc <niranjannlc10@gmail.com>
2022-07-25 08:58:57 +0545

    reterofit di

---
+++ app/src/main/java/org/lniranjan/mvi/di/ReterofitModule.kt
@@ -0,0 +1,54 @@
+package org.lniranjan.mvi.di
+
+import android.app.Application
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import org.lniranjan.mvi.model.Blog
+import org.lniranjan.mvi.reterofit.BlogNetworkEntity
+import org.lniranjan.mvi.reterofit.BlogReterofit
+import org.lniranjan.mvi.reterofit.NetworkMapeer
+import org.lniranjan.mvi.utility.EntityMapper
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ReterofitModule {
+
+    @Singleton
+    @Provides
+    fun provideNetworkMapper(): EntityMapper<BlogNetworkEntity,Blog>
+    {
+        return NetworkMapeer()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson {
+        return GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson:  Gson): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl("https://open-api.xyz/placeholder/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+
+    @Singleton
+    @Provides
+    fun provideBlogService(retrofit: Retrofit.Builder): BlogReterofit {
+        return retrofit
+            .build()
+            .create(BlogReterofit::class.java)
+    }
+
+}
