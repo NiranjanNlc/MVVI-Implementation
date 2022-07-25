c3a2340c92f547071e7eb5e72e7a1b13f1785976
niranjannlc <niranjannlc10@gmail.com>
2022-07-25 08:51:32 +0545

    room setup

--- app/src/main/java/org/lniranjan/mvi/reterofit/BlogReterofit.kt
+++ app/src/main/java/org/lniranjan/mvi/reterofit/BlogReterofit.kt
@@ -2,7 +2,7 @@ package org.lniranjan.mvi.reterofit
 
 import retrofit2.http.GET
 
-interface BlogRetrofit {
+interface BlogReterofit {
 
     @GET("blogs")
     suspend fun get(): List<BlogNetworkEntity>
