4841ed485f182202b2ab296506ea97685b3d2c88
niranjannlc <niranjannlc10@gmail.com>
2022-07-25 09:11:05 +0545

    repository module

---
+++ app/src/main/java/org/lniranjan/mvi/di/RepositoryModule.kt
@@ -0,0 +1,28 @@
+package org.lniranjan.mvi.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import org.lniranjan.mvi.repository.MainRepository
+import org.lniranjan.mvi.reterofit.BlogReterofit
+import org.lniranjan.mvi.reterofit.NetworkMapeer
+import org.lniranjan.mvi.room.BlogCacheMapper
+import org.lniranjan.mvi.room.BlogDao
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+private object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(
+        blogDao: BlogDao,
+        retrofit: BlogReterofit,
+        cacheMapper: BlogCacheMapper,
+        networkMapper: NetworkMapeer
+    ): MainRepository {
+        return MainRepository(blogDao, retrofit, cacheMapper, networkMapper)
+    }
+}
